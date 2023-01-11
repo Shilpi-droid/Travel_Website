ff6bad9a6a22931ddd4a304d9b516a23d2c42b77
Shilpi Giri <202151145@iiitvadodara.ac.in>
2023-01-11 22:02:40 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -5,15 +5,17 @@ import cors from 'cors';
 
 const app=express();
 
-app.use(body-parser.json({limit:"30 mb" , extended: true}));
-app.use(body-parser.urlencoded({limit:"30 mb" , extended: true}));
+app.use(bodyParser.json({limit:"30 mb" , extended: true}));
+app.use(bodyParser.urlencoded({limit:"30 mb" , extended: true}));
 app.use(cors());
 
-const CONNECTION_URL='mongodb+srv://shilpi:<WWTBNvJX9HNdqZvo>@cluster0.zhgui3w.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT ||5000;
+const CONNECTION_URL='mongodb+srv://shilpi:WWTBNvJX9HNdqZvo@cluster0.zhgui3w.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {userNewUrlParser:true,useUnifiedTopology:true})
-    .then(()=> app.listen(PORT, ()=> console.log('Server running on: ${PORT}')))
+mongoose.set('strictQuery',false);
+
+mongoose.connect(CONNECTION_URL, {useUnifiedTopology:true})
+    .then(()=> app.listen(PORT, ()=> console.log(`Server running on: ${PORT}`)))
     .catch((error)=>console.log(error.message));
 
-mongoose.set('useFindAndModify',false);
+//mongoose.set('useFindAndModify',false);
