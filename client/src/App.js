03c28de024869fabb05d29ed59131fe67a77d806
Dhruv Yadav <dhruv.yadav@spit.ac.in>
2023-01-15 13:51:00 +0530

    remove node modules from commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
-import React  from "react";
+import * as React from 'react';
+import Map, {Marker} from 'react-map-gl';
+const REACT_MAPBOX_TOKEN= 'pk.eyJ1Ijoic2hpbHBpNDciLCJhIjoiY2xjdzFtOXRuMXA4aTNybXNjcTY1eXEwbyJ9.fQsMY9DLe57wVnw3yDLJuQ';
+const App = () =>{
+    return (
+        <div className="App">   
+            <Map
+      initialViewState={{
+        latitude: 46,
+        longitude: 17,
+        zoom: 1 
+      }}
+      style={{width: 1500, height: 670}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    //   mapboxAccessToken={process.env.MAPBOX_TOKEN}
+      mapboxAccessToken={REACT_MAPBOX_TOKEN}
+    >
+      <Marker longitude={2} latitude={48} anchor="bottom" >
+      {/* <img src="./pin.png" /> */}
+    </Marker>
+    
+    </Map>
+        </div>
+    )
+}
 
-const App = () => { 
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
-};
 export default App;
